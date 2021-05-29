c60b1cd92b3efe2b32baf64d9a9d20a7df204abf
raaydr <dharmawanreynaldi@gmail.com>
2021-05-29 08:33:36 +0800

    perbaikan migration + tambah tabel peserta

--- database/migrations/2021_05_28_062133_create_table_peserta.php
+++ database/migrations/2021_05_28_062133_create_table_peserta.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTableProfilFasil extends Migration
+class CreateTablePeserta extends Migration
 {
     /**
      * Run the migrations.
@@ -13,19 +13,14 @@ class CreateTableProfilFasil extends Migration
      */
     public function up()
     {
-        Schema::create('fasil', function (Blueprint $table) {
-            
+        Schema::create('peserta', function (Blueprint $table) {
             $table->increments('id');
             $table->string('nama');
-            $table->string('email')->unique();
-            $table->enum('jenis_kelamin', ['Pria', 'Wanita']);
-            $table->string('phonenumber');
-            $table->string('instagram');
-            $table->longText('prestasi');
-            $table->string('quotes');
             $table->boolean('status');
-            $table->text('url_foto');
+            $table->boolean('captain');
             $table->integer('grup');
+            $table->integer('gen');
+            
             $table->integer('user_id')->unsigned();   
             $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
             $table->timestamps();
@@ -39,6 +34,6 @@ class CreateTableProfilFasil extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('fasil');
+        Schema::dropIfExists('peserta');
     }
 }
