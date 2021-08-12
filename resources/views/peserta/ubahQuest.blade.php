8d8287447af6b4df2aa98471b33d332a38d04ff1
raaydr <dharmawanreynaldi@gmail.com>
2021-08-12 16:55:22 +0800

    sidebar peserta

--- resources/views/peserta/ubahQuest.blade.php
+++ resources/views/peserta/ubahQuest.blade.php
@@ -71,66 +71,8 @@
             </ul>
          </nav>
          <!-- /.navbar -->
-         <!-- Main Sidebar Container -->
-         <aside class="main-sidebar sidebar-light-success elevation-4">
-            <!-- Brand Logo -->
-            <a href="/" class="brand-link">
-            <img src="{{asset('develop')}}/img/logo.png" alt="AdminLTE Logo" class="brand-image" style="opacity: 0.8;" />
-            <span class="brand-text font-weight-light">Peserta</span>
-            </a>
-            <!-- Sidebar -->
-            <div class="sidebar">
-               <!-- Sidebar Menu -->
-               <nav class="mt-2">
-                  <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                     <!-- Add icons to the links using the .nav-icon class
-                        with font-awesome or any other icon font library -->
-                     <li class="nav-item">
-                        <a href="{{ route('peserta.pengumuman') }}" class="nav-link">
-                           <i class="nav-icon nav-icon far fa-envelope"></i>
-                           <p>
-                              Pengumuman
-                           </p>
-                        </a>
-                     </li>
-                     <li class="nav-item">
-                        <a href="{{ route('peserta.grup') }}" class="nav-link ">
-                           <i class="nav-icon fas fa-table"></i>
-                           <p>
-                              Grup
-                           </p>
-                        </a>
-                     </li>
-                     <li class="nav-item">
-                        <a href="{{ route('peserta.daily.quest') }}" class="nav-link ">
-                           <i class="nav-icon fas fa-tachometer-alt"></i>
-                           <p>
-                              Daily Quest
-                           </p>
-                        </a>
-                     </li>
-                     <li class="nav-item">
-                        <a href="{{ route('peserta.record.quest') }}" class="nav-link active">
-                           <i class="nav-icon fas fa-edit"></i>
-                           <p>
-                              Quest Record
-                           </p>
-                        </a>
-                     </li>
-                     <li class="nav-item">
-                        <a href="{{ route('peserta.dashboard') }}" class="nav-link ">
-                           <i class="nav-icon fas ion-person"></i>
-                           <p>
-                              Dashboard
-                           </p>
-                        </a>
-                     </li>
-                  </ul>
-               </nav>
-               <!-- /.sidebar-menu -->
-            </div>
-            <!-- /.sidebar -->
-         </aside>
+          <!-- Main Sidebar Container -->
+          @include('peserta.sidebar')
          <!-- Content Wrapper. Contains page content -->
          <div class="content-wrapper">
             <!-- Content Header (Page header) -->
