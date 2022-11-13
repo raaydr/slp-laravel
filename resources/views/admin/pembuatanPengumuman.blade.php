c211b212fd658e39b8a4530c27d96994e099604c
raaydr <dharmawanreynaldi@gmail.com>
2022-11-13 13:21:15 +0800

    perbaikan pada halaman pembuatan pengumuman

--- resources/views/admin/pembuatanPengumuman.blade.php
+++ resources/views/admin/pembuatanPengumuman.blade.php
@@ -5,12 +5,12 @@
                <div class="container-fluid">
                   <div class="row mb-2">
                      <div class="col-sm-6">
-                        <h1>Pembuatan Laporan</h1>
+                        <h1>Pembuatan Pengumuman</h1>
                      </div>
                      <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                            <li class="breadcrumb-item"><a href="/">Admin</a></li>
-                           <li class="breadcrumb-item active">Pembuatan-Laporan</li>
+                           <li class="breadcrumb-item active">Pembuatan-Pengumuman</li>
                         </ol>
                      </div>
                   </div>
@@ -30,7 +30,7 @@
                      <!-- general form elements -->
                      <div class="card card-primary">
                         <div class="card-header">
-                           <h3 class="card-title">Form Laporan</h3>
+                           <h3 class="card-title">Form Pengumuman</h3>
                         </div>
                         <!-- /.card-header -->
                         <!-- form start -->
@@ -117,7 +117,7 @@
                   <div class="col-12">
                               <div class="card">
                                  <div class="card-header">
-                                       <h3 class="card-title">Target Tugas List</h3>
+                                       <h3 class="card-title">List Pengumuman</h3>
                                  </div>
                                  <!-- /.card-header -->
                                  <div class="card-body">
