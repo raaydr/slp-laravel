ca288e82dbd7878fa7e924836bb4a59ab40cb453
raaydr <dharmawanreynaldi@gmail.com>
2023-01-16 11:07:55 +0800

    ubah dikit

--- resources/views/master/validasiTugas.blade.php
+++ resources/views/master/validasiTugas.blade.php
@@ -7,7 +7,7 @@
                      </div>
                      <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
-                           <li class="breadcrumb-item"><a href="/">Admin</a></li>
+                           <li class="breadcrumb-item"><a href="/">{{ ucfirst(getMyPermission(Auth::user()->level)) }}</a></li>
                            <li class="breadcrumb-item active">Validasi Tugas</li>
                         </ol>
                      </div>
